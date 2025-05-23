124f49ec379501b6dc992bd50862a08a1b6e0226
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2025-05-23 06:26:59 +0530

    init

---
+++ next-app/app/page.tsx
@@ -0,0 +1,25 @@
+import { Todos } from "@/todos/todos";
+import { Todo } from "@/types/Todo.types";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <div className="border border-violet-500 p-4 space-y-2">
+      <h1>My <span className="font-semibold bg-gradient-to-r from-violet-500 to-purple-500 text-transparent bg-clip-text"> TodoList </span> Test</h1>
+      <p className="text-sm font-thin">TodoList consists of various tasks</p>
+      <div>
+        {Todos.map((todo: Todo, index: number)=>{
+        return  <div key={index} className="space-y-1">
+          <div>
+          {todo.id}. <span className="bg-gradient-to-r from-violet-500 to-purple-500 text-transparent bg-clip-text">{todo.title}</span>
+          </div>
+          <div className="font-medium">
+           - {todo.description}
+          </div>
+         </div>
+        })}
+      </div>
+      </div>
+    </div>
+  );
+}
---
+++ next-app/todos/todos.ts
@@ -0,0 +1,22 @@
+export const Todos = [
+    {
+        id:1,
+        title:"Eating",
+        description:"Very important task"
+    },
+    {
+        id: 2,
+        title:"Running",
+        description:"I usually go in the morning"
+    },
+    {
+        id:3,
+        title:"Scrolling on yt",
+        description:"I do when i get bored"
+    },
+    {
+        id:4,
+        title:"Fun with friends",
+        description:"I do whenever i get free"
+    }
+]
